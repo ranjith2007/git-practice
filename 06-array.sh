14b5675c9a5db9fb51a9df7ae0257bed2dc5898c
ranjith2007 <ranjithkumar0799@gmail.com>
2024-12-16 20:57:25 +0530

    added array

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "KIWI" "ORANGE")
+
+echo "First fruit is: ${FRUITS[0]}"
+echo "SECOND fruit is: ${FRUITS[1]}"
+echo "THIRD fruit is: ${FRUIT[2]}"
+
+
+echo "First fruit is: ${Fruit[@]}"
