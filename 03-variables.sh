bff67948066326ddce8ac691df90113b165852e6
ranjith2007 <ranjithkumar0799@gmail.com>
2024-12-16 18:38:33 +0530

    added variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Ranjith
+PERSON2=Kumar
+
+echo "$PERSON1:: Hi ${PERSON2}, How are you"
+echo "$PERSON2:: Hello ${PERSON1}, I am fine. How are you"
+echo "$PERSON1:: I am doing good ${PERSON2}. "
